7e66475f1ace872ec2b1536f6ada9950f64d4a43
sreedhar eg <sreedhareg1997@gmail.com>
2025-09-24 17:01:39 +0530

    initial commit for drag and drop project

--- Drag and Drop Project/src/app.ts
+++ Drag and Drop Project/src/app.ts
@@ -1,1 +1,26 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-input")
+    ); // one way of type casting useing <>, i.e <HTMLTemplateElement>
+    this.hostElement = document.getElementById("app") as HTMLDivElement; // another way of type casting useing 'as'
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+new ProjectInput();
